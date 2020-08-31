3c1c6e91df4a3fccf82f52865cea90f5da54d520
JeffreyThompson <jeffathomp@gmail.com>
2020-08-31 11:11:31 -0500

    add jquery, alter pocket order, start on objects

---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+console.log('Javascript is working');
+
+const playerOne = {
+    score: 0,
+    stones: 24,
+    name: 'Player 1'
+};
+
+const playerTwo = {
+    score: 0,
+    stones: 24,
+    name: 'Player 2'
+};
+
+const player1Pocket1 = {
+    name: 'player1Pocket1',
+};
+
+const game = {
+    activePlayer: playerOne,
+    passivePlayer: playerTwo,
+    start() {
+
+    },
+    scoreboard() {
+
+    },
+    changePlayer() {
+
+    },
+    gameOver() {
+
+    }
+};
