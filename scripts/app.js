39b2d4c5429a7bc92bf4d4cea212bca71e594dbe
JeffreyThompson <jeffathomp@gmail.com>
2020-08-31 11:38:19 -0500

    create pocket objects

--- scripts/app.js
+++ scripts/app.js
@@ -14,6 +14,111 @@ const playerTwo = {
 
 const player1Pocket1 = {
     name: 'player1Pocket1',
+    owner: playerOne,
+    numStones: 0,
+    stones: [],
+    fromStore: 1,
+    $location: $('#player1-pocket1')
+};
+
+const player1Pocket2 = {
+    name: 'player1Pocket2',
+    owner: playerOne,
+    numStones: 0,
+    stones: [],
+    fromStore: 2,
+    //DOM location
+    $location: $('#player1-pocket2')
+};
+
+const player1Pocket3 = {
+    name: 'player1Pocket3',
+    owner: playerOne,
+    numStones: 0,
+    stones: [],
+    fromStore: 3,
+    $location: $('#player1-pocket3')
+};
+
+const player1Pocket4 = {
+    name: 'player1Pocket4',
+    owner: playerOne,
+    numStones: 0,
+    stones: [],
+    fromStore: 4,
+    $location: $('#player1-pocket4')
+};
+
+const player1Pocket5 = {
+    name: 'player1Pocket5',
+    owner: playerOne,
+    numStones: 0,
+    stones: [],
+    fromStore: 5,
+    $location: $('#player1-pocket5')
+};
+
+const player1Pocket6 = {
+    name: 'player1Pocket6',
+    owner: playerOne,
+    numStones: 0,
+    stones: [],
+    fromStore: 6,
+    $location: $('#player1-pocket6')
+};
+
+const player2Pocket1 = {
+    name: 'player2Pocket1',
+    owner: playerTwo,
+    numStones: 0,
+    stones: [],
+    fromStore: 1,
+    $location: $('#player2-pocket1')
+};
+
+const player2Pocket2 = {
+    name: 'player2Pocket2',
+    owner: playerTwo,
+    numStones: 0,
+    stones: [],
+    fromStore: 2,
+    $location: $('#player2-pocket2')
+};
+
+const player2Pocket3 = {
+    name: 'player2Pocket3',
+    owner: playerTwo,
+    numStones: 0,
+    stones: [],
+    fromStore: 3,
+    $location: $('#player2-pocket3')
+};
+
+const player2Pocket4 = {
+    name: 'player2Pocket4',
+    owner: playerTwo,
+    numStones: 0,
+    stones: [],
+    fromStore: 4,
+    $location: $('#player2-pocket4')
+};
+
+const player2Pocket5 = {
+    name: 'player2Pocket5',
+    owner: playerTwo,
+    numStones: 0,
+    stones: [],
+    fromStore: 5,
+    $location: $('#player2-pocket5')
+};
+
+const player2Pocket6 = {
+    name: 'player2Pocket6',
+    owner: playerTwo,
+    numStones: 0,
+    stones: [],
+    fromStore: 1,
+    $location: $('#player2-pocket6')
 };
 
 const game = {
